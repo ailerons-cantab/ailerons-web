46ec6c9de43d206f6e56c1e0db071968e5360634
Alex90Jennings <aj0312@my.bristol.ac.uk>
2026-01-25 12:29:47 -0300

    footer loaded once

--- src/app/(main)/home/layout.tsx
+++ src/app/(main)/home/layout.tsx
@@ -1,18 +1,11 @@
-import Footer from "@/app/footer";
 import Header from "@/app/header";
 
-
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
-
-    //layout on /home/*
-
-    console.log("HomeLayout rendered");
 
     return (
         <div className="min-h-screen bg-white text-gray-900">
             <Header />
             <main>{children}</main>
-            <Footer />
         </div>
     );
 }
